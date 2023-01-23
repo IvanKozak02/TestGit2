99d60be33d2b4e9f915e3d7f0a999b9da4bcf18d
Ivan Kozak <ivasykkozak611@gmail.com>
2023-01-23 19:47:21 +0200

    some changes

--- src/main/java/com/kozak/Main.java
+++ src/main/java/com/kozak/Main.java
@@ -2,6 +2,6 @@ package com.kozak;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Ivan!!!");
+        System.out.println("Hello Iv!!!");
     }
 }
